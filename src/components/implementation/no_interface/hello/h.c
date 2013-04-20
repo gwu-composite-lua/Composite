437db4628fb6381aa4217164f97414c04239aabd
Chris Anderson <christopher.anderson@gmail.com>
2013-04-20 12:27:26 -0700

    Hello world Lua component finished can push and retrieve value from the lua stack

--- src/components/implementation/no_interface/hello/h.c
+++ src/components/implementation/no_interface/hello/h.c
@@ -7,13 +7,17 @@
 void
 cos_init(void)
 {
-	printc("Hello, dinosaur!");
+	printc("We welcome our new Lua overlords!");
+	// Creates the lua state
 	lua_State *L = luaL_newstate();
-	int top = lua_gettop(L);
 
-	printc("Lua stack is %d", top);
+	// Pushes an int onto the Lua stack, pops it, 
+	// then prints it out
+	lua_pushnumber(L, 23);
+	int num = (int)lua_tonumber(L,1);
+	printc("Lua stack is %d\n", num);
 
-
+	// Closes lua
 	lua_close(L);
 	printc("END OF LUA FILE\n");
 
