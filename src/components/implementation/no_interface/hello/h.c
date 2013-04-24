f94bcea631924231199833fbf6850d4aeb8dcd64
Chris Anderson <christopher.anderson@gmail.com>
2013-04-23 21:17:00 -0700

    Testing loading from a file and executing lua strings

--- src/components/implementation/no_interface/hello/h.c
+++ src/components/implementation/no_interface/hello/h.c
@@ -3,11 +3,15 @@
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
+#include <stdlib.h>
+
+int map_create(lua_State *lua);
+int map_slice(lua_State *lua);
 
 void
 cos_init(void)
 {
-	printc("We welcome our new Lua overlords!");
+	printc("We welcome our new Lua overlords!\n");
 	// Creates the lua state
 	lua_State *L = luaL_newstate();
 
@@ -17,9 +21,40 @@ cos_init(void)
 	int num = (int)lua_tonumber(L,1);
 	printc("Lua stack is %d\n", num);
 
+	// Testing executing a string of Lua
+	// returns a value which pushes it onto the stack, then
+	// prints it
+	luaL_dostring(L, "return 'with spaces something'");
+	const char *str = lua_tostring(L, -1);
+	printc("Lua returned value is %s", str);
+	
+	// tests loding configuration from a file
+	// loads the file, then executes it, which converts the variables
+	// in the file to lua parsable data
+	if (luaL_loadfile(L, "/home/cos/research/composite/src/components/implementation/no_interface/hello/test.txt") || lua_pcall(L, 0, 0, 0)){
+		// if either call fails, will return non-zero error code and print
+		// it out here
+		printc("error %s", lua_tostring(L, -1));
+	}
+
+	lua_getglobal(L, "width");
+	int width = lua_tonumber(L, -1);
+	printc("Width is %d\n", width);
+
 	// Closes lua
 	lua_close(L);
 	printc("END OF LUA FILE\n");
 
 	return;
 }
+
+int map_create(lua_State *lua)
+{
+	printc("mape create");
+}
+
+int map_slice(lua_State *lua)
+{
+	printc("map slice");
+}
+
