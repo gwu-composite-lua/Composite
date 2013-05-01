4972ee1410de3c620ce30a8bde03e07afe9e9e09
knowlt15 <knowlt15@gmail.com>
2013-04-30 19:23:25 -0700

    small logging tweaks

--- src/components/implementation/lua_keyval/luakv/lkv.c
+++ src/components/implementation/lua_keyval/luakv/lkv.c
@@ -27,15 +27,12 @@ lua_State *L;
 char* luakv_getstring_bytable (char *key, char *table_name) 
 {
 	char *result;
-	printc("try to get globaltable\n");
 	lua_getglobal(L, table_name);
-	printc("check to see if it is actually a table");
 	if(!lua_istable(L, -1))
 	{
-		printc("tablespace didn't exist so try to create\n");
+		printc("==== tablespace didn't exist so try to create\n");
 		create_tablespace(table_name);
 	}
-	printc("looks like we should have our table now\n");
 	lua_pushstring(L, key);
 	lua_gettable(L, -2);
 	result = (char*)lua_tostring(L, -1);
@@ -116,6 +113,7 @@ void luakv_putnumber (char *key, int intvalue)
 */
 void create_tablespace (char *table_name)
 {
+	printc("=== Attempting to create new lua table with table_name=%s\n",table_name);
 	// Create the new table and assign it to global name *table_name
 	lua_newtable(L);
 	lua_setglobal(L, table_name);
@@ -125,46 +123,46 @@ void create_tablespace (char *table_name)
 
 void cos_init(void)
 {
-	printc("Trying out some keyval operations\n");
+	printc("=== Trying out some keyval operations\n");
 	// Creates the lua state
 	L = luaL_newstate();
-	printc("Starting lua keyvalue storage test. New Lua state has been set.\n");
+	printc("=== Starting lua keyvalue storage test. New Lua state has been set.\n");
 	// Test for an empty value
 	char *nilField = luakv_getstring("EMPTY");
-	printc("trying to getString before fields are entered. received %s\n", nilField);
+	printc("=== trying to getString before fields are entered. received %s\n", nilField);
 	// Test putting a string value and retrieving it
 	luakv_putstring("key1","value1");
-	printc("Put string of value1 into the default table at key1\n");
+	printc("=== Put string of value1 into the default table at key1\n");
 	char *get1 = luakv_getstring("key1");
-	printc("Retrieved the value at key1, return was: %s\n", get1);
+	printc("=== Retrieved the value at key1, return was: %s\n", get1);
 	// Test putting a number value and retrieving it
 	luakv_putnumber("key2",(int)2);
-	printc("Put number value of 2 into the default table at key2\n");
+	printc("=== Put number value of 2 into the default table at key2\n");
 	int get2 = luakv_getnumber("key2");
-	printc("pull from the table key2: %d\n", get2);
+	printc("=== pull from the table key2: %d\n", get2);
 	// Try storing values into another tablespace
 	luakv_putstring_bytable("key3","value3","TABLE2");
 	luakv_putnumber_bytable("key4",(int)4,"TABLE2");
-	printc("Placed key3=value3 and key4=4 into TABLE2\n");
+	printc("=== Placed key3=value3 and key4=4 into TABLE2\n");
 	// Try to retrieve the other tables valus from the default table
-	printc("attempting to retrieve TABLE2 values from DEFAULT table\n");	
+	printc("=== attempting to retrieve TABLE2 values from DEFAULT table\n");	
 	char *get3_fail = luakv_getstring("key3");
 	int get4_fail = luakv_getnumber("key4");
-	printc("should have no values returned, retrieved key3=%s and key4=%d\n",get3_fail,get4_fail);
+	printc("=== should have no values returned, retrieved key3=%s and key4=%d\n",get3_fail,get4_fail);
 	// Now get TABLE 2 values from table2
 	char *get3 = luakv_getstring_bytable("key3", "TABLE2");
 	int get4 = luakv_getnumber_bytable("key4", "TABLE2");
-	printc("should now have returned values from table 2 key3=%s and key4=%d\n",get3, get4);
+	printc("=== should now have returned values from table 2 key3=%s and key4=%d\n",get3, get4);
 	// Test duplicate key but in different table
 	luakv_putstring("key3","default_value3");
-	printc("stored key3=default_value3 in default table\n");
+	printc("=== stored key3=default_value3 in default table\n");
 	char *default3 = luakv_getstring("key3");
 	char *bytable3 = luakv_getstring_bytable("key3", "TABLE2");
-	printc("Pulled key3 from both tables. DEFAULT=%s and TABLE2=%s\n", default3, bytable3);
+	printc("=== Pulled key3 from both tables. DEFAULT=%s and TABLE2=%s\n", default3, bytable3);
 	
 	// Closes lua
 	lua_close(L);
-	printc("lua_state has been closed\n");
+	printc("=== lua_state has been closed\n");
 	return;
 }
 
